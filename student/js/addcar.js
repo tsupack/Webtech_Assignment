df87bd6f4ba866c89d259174d9050911de769ffe
Nussbaumer Alex <nuss.alex96@gmail.com>
2018-11-09 22:55:31 +0100

    Moved selector script to addcar.js.

--- student/js/addcar.js
+++ student/js/addcar.js
@@ -1,3 +1,14 @@
+$(document).ready(function () {
+    $.getJSON('manufacturerNames', function (data) {
+        var select = $('<select name="manufacturer" required="required"></select>');
+        $.each(data, function (key, value) {
+            var option = $('<option value="' + value+ '">' + value + '</option>');
+            select.append(option);
+        });
+        $(".select").append(select);
+    });
+});
+
 $(function (){
     $('form').on('submit', function (submit) {
         submit.preventDefault();
